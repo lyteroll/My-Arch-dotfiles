30d1bd97fe60fe272e7e2cfcca16312f7f32b5e8
lyteroll <lyterol@protonmail.com>
2026-02-04 16:07:19 +0100

    added nvim dotfiles

---
+++ .config/nvim/lua/plugins/oneliners.lua
@@ -0,0 +1,17 @@
+return {
+    { -- This helps with ssh tunneling and copying to clipboard
+	'ojroques/vim-oscyank',
+    },
+
+    {
+      -- Git plugin
+	'tpope/vim-fugitive',
+    },
+    {
+      -- CSS Colors shown plugin
+	'brenoprata10/nvim-highlight-colors',
+	config = function()
+	    require('nvim-highlight-colors').setup({})
+	end
+    },
+}
