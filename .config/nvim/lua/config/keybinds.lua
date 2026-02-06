362ee52994443589ef28692ea9592424b2943da2
lyteroll <lyterol@protonmail.com>
2026-02-06 12:43:03 +0100

    config changes

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>y", "+y")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
 vim.keymap.set("n", "<leader>n", ":nohlsearch<CR>")
 
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- search and replace
